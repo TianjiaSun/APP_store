fefc926a397755dea8d0839d76ecd0dcca0267f0
TianjiaSun <thomas.sun@outlook.com>
2015-03-12 20:52:44 -0700

    change shape

--- app/js/display.js
+++ app/js/display.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   $("#overlay_container").hide();
 
   $(".app_unit").click(function() {
-    $("#overlay_container").show();   
+    $("#overlay_container").fadeIn(500);   
+  });
+
+  $(".overlay_close").click(function() {
+    $("#overlay_container").hide();   
   });
 })
